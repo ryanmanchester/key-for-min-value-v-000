45c6097d50300f974f4b580aad3c02bde6a8c661
Ryan Manchester <ryanm17@gmail.com>
2017-10-22 18:39:13 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
     nil
   end
   min_value = nil
-  min_key = nil 
+  min_key = nil
     name_hash.each do |key, value|
       if min_value == nil
         min_key = key
